05f60a8487d56cc09b0034835fe24917e997f1d5
SirPhemmiey <oluwafemiakinde@gmail.com>
2022-08-25 00:57:17 +0400

    feat: finish up sudoku class

--- source/business/Game/Game.interfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GameType = void 0;
-var GameType;
-(function (GameType) {
-    GameType["Sudoku"] = "sudoku";
-})(GameType = exports.GameType || (exports.GameType = {}));
--- source/helpers/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.messages = void 0;
-exports.messages = {
-    SUCCESS: "success",
-    FAILED: "failed",
-};
